62a29eac42b65194bce3e615418f5b9357cf463a
karthikbadam <sbadam@purdue.edu>
2015-01-02 23:52:02 -0500

    using deap2

--- forecast.py
+++ forecast.py
@@ -1,4 +1,20 @@
-import random, array 
+import random,array
 from deap import creator, base, tools, algorithms
+import fileinput
+import glob
+import re
+import os
+import pprint as pp
+
+paths = ["../data/GOOG.csv"]
+
+stockValues = []
+
+for path in paths:
+    for f in glob.glob(path):
+        fo = open(f)
+        lines = fo.readlines()
+        for line in lines:
+            contents = line.split(",")
 
 
